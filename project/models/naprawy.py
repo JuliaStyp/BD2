a5c20feb5b2011e0b63246ef7e27f50d83b184a9
JuliaStyp <julia.styp@gmail.com>
2024-05-11 16:20:52 +0200

    naprawy constraints

--- project/models/naprawy.py
+++ project/models/naprawy.py
@@ -1,4 +1,12 @@
-from sqlalchemy import Column, Integer, String, Date, Numeric, ForeignKey
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Date,
+    Numeric,
+    ForeignKey,
+    CheckConstraint,
+)
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -20,19 +28,22 @@ class Naprawa(Base):
 
 class PowodNaprawy(Base):
     __tablename__ = "powody_naprawy"
+    __table_args__ = CheckConstraint("przeglad_id IS NULL <> zgloszenie_id IS NULL")
 
     id = Column(Integer, primary_key=True)
-    przeglad_id = Column(Integer, ForeignKey("przeglady.id"))
-    zgloszenie_id = Column(Integer, ForeignKey("zgloszenia_naprawy.id"))
-    priorytet = Column(Integer)
-    uwagi = Column(String(1024))
+    przeglad_id = Column(Integer, ForeignKey("przeglady.id"), nullable=True)
+    zgloszenie_id = Column(
+        Integer, ForeignKey("zgloszenia_naprawy.id"), nullable=True, unique=True
+    )
+    priorytet = Column(Integer, nullable=False)
+    uwagi = Column(String(1024), nullable=True)
 
 
 class ZgloszenieNaprawy(Base):
     __tablename__ = "zgloszenia_naprawy"
 
     id = Column(Integer, primary_key=True)
-    element_id = Column(Integer, ForeignKey("elementy.id"))
-    data = Column(Date)
-    zglaszajacy = Column(String(64))
-    uwagi = Column(String(128))
+    element_id = Column(Integer, ForeignKey("elementy.id"), nullable=False)
+    data = Column(Date, nullable=False)
+    zglaszajacy = Column(String(64), nullable=False)
+    uwagi = Column(String(128), nullable=True)
