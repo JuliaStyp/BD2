555aff5342dae12ef1d391cdc8e1076bc56f68a2
JuliaStyp <julia.styp@gmail.com>
2024-05-11 18:16:48 +0200

    comment

--- project/app.py
+++ project/app.py
@@ -4,5 +4,5 @@ from models import Base
 if __name__ == "__main__":
     engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
     Base.metadata.bind = engine
-    Base.metadata.drop_all(engine)
+    # Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
