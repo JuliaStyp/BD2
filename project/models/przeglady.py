9f0481182c254096a13c38243440d2984d4895b4
Maciej Dobrowolski <maciejd644@gmail.com>
2024-05-11 16:27:32 +0200

    dodano wielkosci string, kilka poprawek

--- project/models/przeglady.py
+++ project/models/przeglady.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, ForeignKey, DECIMAL
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Numeric
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -8,9 +8,9 @@ class Serwisant(Base):
     __tablename__ = "serwisanci"
 
     id = Column(Integer, primary_key=True)
-    nazwa = Column(String, nullable=False)
-    nr_telefonu = Column(String, nullable=False)
-    adres_mail = Column(String, nullable=False)
+    nazwa = Column(String(128), nullable=False)
+    nr_telefonu = Column(String(32), nullable=False)
+    adres_mail = Column(String(128), nullable=False)
 
 
 class Przeglad(Base):
@@ -20,25 +20,25 @@ class Przeglad(Base):
     typ_przegladu_id = Column(Integer, ForeignKey("typy_przegladow.id"), nullable=False)
     serwisant_id = Column(Integer, ForeignKey("serwisanci.id"), nullable=False)
     powod_id = Column(Integer, ForeignKey("powody.id"), nullable=False)
-    opis_zakresu_prac = Column(String, nullable=False)
+    opis_zakresu_prac = Column(String(1024), nullable=False)
     data_rozpoczecia = Column(Date, nullable=False)
     data_zakonczenia = Column(Date, nullable=True)
-    koszt = Column(Integer, , nullable=False) # czy jest cos takiego jak decimal?
+    koszt = Column(Numeric(precision=2), nullable=False)
 
 
 class TypPrzegladu(Base):
     __tablename__ = "typy_przegladow"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    typ = Column(String, nullable=False)
+    typ = Column(String(128), nullable=False)
 
 
 class PowodPrzegladu(Base):
     __tablename__ = "powody_przegladow"
 
     id = Column(Integer, primary_key=True)
-    powod = Column(String)
-    zgloszenie_id = Column(Integer, ForeignKey("zgloszenia_przegladow.id"), unique=True)
+    powod = Column(String(128), nullable=False)
+    zgloszenie_id = Column(Integer, ForeignKey("zgloszenia_przegladow.id"), unique=True, nullable=False)
 
 
 class SprawdzoneElementy(Base):
@@ -53,7 +53,7 @@ class ZgloszeniePrzegladu(Base):
 
     id = Column(Integer, primary_key=True)
     data = Column(Date, nullable=False)
-    zglaszajacy = Column(String, nullable=False)
-    uwagi = Column(String, nullable=True)
+    zglaszajacy = Column(String(32), nullable=False)
+    uwagi = Column(String(128), nullable=True)
     element_id = Column(Integer, ForeignKey("elementy_infrastruktury.id"), nullable=False)
 
