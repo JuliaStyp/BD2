d8d87051f549d8449cfa56380ab3c7cfb0f4b6bf
Michal <michal.golawski.mg@gmail.com>
2024-05-11 15:31:54 +0200

    Dodano modele dotyczące infrastruktury

---
+++ project/models/infrastruktura.py
@@ -0,0 +1,54 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Boolean, Float
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class ElementInfrastruktury(Base):
+    __tablename__ = "elementy_infrastruktury"
+
+    id = Column(Integer, primary_key=True)
+    typ = Column(Integer, ForeignKey("typy_infrastruktury.id"), nullable=False)
+    lokalizacja = Column(Integer, ForeignKey("lokalizacje.id"), nullable=False)
+    obiekt = Column(Integer, ForeignKey("obiekty.id"), nullable=False)
+    status = Column(Integer, ForeignKey("statusy_elementow.id"), nullable=False)
+    opis = Column(String, nullable=False)
+    max_interwal = Column(Integer, nullable= False)
+
+
+class TypInfrastruktury(Base):
+    __tablename__ = "typy_infrastruktury"
+
+    id = Column(Integer, primary_key=True)
+    typ = Column(String, nullable=False)
+
+
+class Obiekt(Base):
+    __tablename__ = "obiekty"
+
+    id = Column(Integer, primary_key=True)
+    typ = Column(String, nullable=False)
+    do_uzytku = Column(Boolean, nullable=False)
+
+
+class StatusElementu(Base):
+    __tablename__ = "statusy_elementow"
+
+    id = Column(Integer, primary_key=True)
+    status = Column(String, nullable=False)
+
+
+class Lokalizacja(Base):
+    __tablename__ = "lokalizacje"
+
+    id = Column(Integer, primary_key=True)
+    szerokosc_geo = Column(Float, nullable=False)
+    dlugosc_geo = Column(Float, nullable=False)
+    nazwa_miejsca = Column(String, nullable=True)
+
+
+
+
+
+
+
+
