67287a981692e88ba26c65028da33e86f24d7d39
Michal <michal.golawski.mg@gmail.com>
2024-05-15 17:52:21 +0200

    Dodano formularz do dodawania zgłoszenia potrzeby naprawy

--- project/forms/repair_forms.py
+++ project/forms/repair_forms.py
@@ -1,6 +1,7 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, IntegerField, TextAreaField
+from wtforms import Form, BooleanField, StringField, PasswordField, IntegerField, TextAreaField,\
+                    DateField
 from wtforms.validators import NumberRange, InputRequired, Length, Optional
-from db import db, Rola, Uzytkownik, ZgloszenieNaprawy, Przeglad, PowodNaprawy
+from db import db, Rola, Uzytkownik, ZgloszenieNaprawy, Przeglad, PowodNaprawy, ElementInfrastruktury
 
 class RepairReasonForm(Form):
     inspection_id = IntegerField("Id przeglądu", validators=[Optional()])
@@ -8,13 +9,13 @@ class RepairReasonForm(Form):
     priority = IntegerField("Priorytet",
                             validators=[NumberRange(min=0, max=5, message="Priorytet musi być z zakresu <1,5>"),
                                         InputRequired()])
-    remarks = TextAreaField("Uwagi", validators=[Length(min=0, max=1024, message="Uwagi mogą zawierać maksymalnie 1024 znaków")])
+    remarks = TextAreaField("Uwagi",
+                            validators=[Length(min=0, max=1024, message="Uwagi mogą zawierać maksymalnie 1024 znaków")])
 
 
     def validate_inspection_id(form, self):
         if self.data is not None:
-            inspection = db.session.query(Przeglad).filter_by(id=self.data).first()
-            if not inspection:
+            if not db.session.query(Przeglad).filter_by(id=self.data).exists():
                 self.errors.append("Nie istnieje przegląd o podanym id")
                 return False
         return True
@@ -22,13 +23,12 @@ class RepairReasonForm(Form):
 
     def validate_repair_need_report_id(form, self):
         if self.data is not None:
-            repair_need_report = db.session.query(ZgloszenieNaprawy).filter_by(id=self.data).first()
-            if not repair_need_report:
+            if not db.session.query(ZgloszenieNaprawy).filter_by(id=self.data).exists():
                 self.errors.append("Nie istnieje zgłoszenie potrzeby naprawy o podanym id")
                 return False
-            existing_repair_reason = db.session.query(PowodNaprawy).filter_by(zgloszenie_id=self.data).first()
-            if existing_repair_reason:
-                self.errors.append("Zgłoszenie potrzeby naprawy o podanym id zostało już przypisane do istniejącej naprawy")
+            if db.session.query(PowodNaprawy).filter_by(zgloszenie_id=self.data).exists():
+                self.errors.append(
+                    "Zgłoszenie potrzeby naprawy o podanym id zostało już przypisane do istniejącej naprawy")
                 return False
         return True
 
@@ -39,5 +39,34 @@ class RepairReasonForm(Form):
         if ((self.inspection_id.data is not None and self.repair_need_report_id.data is not None) or
             (self.inspection_id.data is None and self.repair_need_report_id.data is None)):
             success = False
-            self.form_errors.append("Musisz wybrać id zgłoszenia potrzeby naprawy lub id przeglądu, nie możesz wybrać obu na raz")
+            self.form_errors.append
+            ("Musisz wybrać id zgłoszenia potrzeby naprawy lub id przeglądu, nie możesz wybrać obu na raz")
         return success and super_succes
+
+
+class RepairNeedReportForm(Form):
+    element_id = IntegerField("Id elementu infrastruktury, który potrzebuje naprawy",
+                              validators=[InputRequired()])
+    date = DateField("Data zgłoszenia potrzeby naprawy",
+                     validators=[InputRequired()])
+    reporting_person = StringField("Osoba zgłaszająca",
+                                   validators=
+                                   [InputRequired(),
+                                    Length(min=0, max=1024,
+                                           message="Osoba zgłaszająca może zawierać maksymalnie 64 znaków")])
+    remarks = TextAreaField("Uwagi",
+                            validators=
+                            [Length(min=0, max=128, message="Uwagi mogą zawierać maksymalnie 128 znaków")])
+
+
+    def validate_element_id(form, self):
+        if not db.session.query(ElementInfrastruktury).filter_by(id=self.data).exists():
+            self.errors.append("Nie istnieje element infrastruktury o podanym id")
+            return False
+        else:
+            return True
+
+
+
+
+
