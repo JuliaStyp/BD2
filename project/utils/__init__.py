f3490912e4bdcf14f75760393e8cd98d9ff25571
msikorsk <01178592@pw.edu.pl>
2024-05-27 17:06:05 +0200

    Added a decorator that checks whether site requires login/admin permissions

---
+++ project/utils/__init__.py
@@ -0,0 +1,3 @@
+from .decorators import admin_required, login_required
+
+__all__ = ["admin_required", "login_required"]
---
+++ project/utils/decorators.py
@@ -0,0 +1,23 @@
+from flask import render_template, session
+from functools import wraps
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get("is_admin"):
+            # Display message instead of redirecting
+            return render_template('error.html', message="Dostęp do strony wymaga uprawnień admina")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get("logged_in"):
+            # Display message instead of redirecting
+            return render_template('error.html', message="Dostęp do strony wymaga zalogowania")
+        return f(*args, **kwargs)
+
+    return decorated_function
