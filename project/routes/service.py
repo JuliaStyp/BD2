e0829a06524bb9ef5690d36ee81bd4b5cca07f30
msikorsk <01178592@pw.edu.pl>
2024-05-30 18:39:16 +0200

    Added login and admin decorators to service and reports routes

--- project/routes/service.py
+++ project/routes/service.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from project.db import db, Serwisant
 from project.forms.service_form import ServiceForm
+from project.utils import login_required, admin_required
 
 service_bp = Blueprint("service_bp", __name__, url_prefix="/service")
 
 @service_bp.route("/")
+@login_required
 def service():
     services = db.session.execute(db.select(Serwisant).order_by(Serwisant.id)).scalars()
     return render_template("service/service.html", services=services)
 
 
 @service_bp.route("/create", methods=["GET", "POST"])
+@login_required
 def create_service():
     context = {}
     if request.method == "POST":
@@ -33,6 +36,8 @@ def create_service():
 
 
 @service_bp.route("/delete/<id>", methods=["POST"])
+@login_required
+@admin_required
 def delete_service(id):
     try:
         service = db.session.query(Serwisant).filter_by(id=id).first()
